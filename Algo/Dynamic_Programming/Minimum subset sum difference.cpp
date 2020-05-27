3dc6a005bba1682c33ce888ec94fec45e407f3e8
SandeepMaithani <sandeep1137@gmail.com>
2020-05-28 01:13:34 +0530

    Minimum subset sum difference

--- Algo/Dynamic_Programming/Minimum subset sum difference.cpp
+++ Algo/Dynamic_Programming/Minimum subset sum difference.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h> 
 using namespace std;
 
+//Bottom - Up Approach
+
 int main() {
 	int n,range = 0,mindiff = INT_MAX;
 	cin>>n;
@@ -50,3 +52,84 @@ int main() {
 	return 0;
 	
 } 
+
+
+//Recursion + Memoization Approach
+
+/*
+
+bool subset(vector<int>&num,vector<vector<int>>&dp,int sum,int n) {
+	if(sum == 0) {
+		return true;
+	}
+	
+	if(sum != 0 && n == 0) {
+		return false;
+	}
+	
+	if(dp[n][sum] != -1) {
+		if(dp[n][sum]) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	if(num[n-1] <= sum) {
+		bool pick = subset(num,dp,sum-num[n-1],n-1);
+		bool drop =	subset(num,dp,sum,n-1);
+		if (pick || drop) {
+			dp[n][sum] = 1;			
+		}
+		else {
+			dp[n][sum] = 0;
+		}
+		
+		return (pick || drop);
+	}
+	else {
+		
+		if (subset(num,dp,sum,n-1)) {
+			dp[n][sum] = 1;			
+		}
+		else {
+			dp[n][sum] = 0;
+		}
+		
+		if(dp[n][sum]) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+int main() {
+	int n,range = 0,mindiff = INT_MAX;
+	cin>>n;
+	
+	vector<int>num(n);
+	
+	for(int i=0;i<n;i++) {
+		cin>>num[i];
+		range += num[i];
+	}
+	
+	vector<vector<int>>dp(n+1,vector<int>(range+1,-1));
+	
+	for(int i=0;i<=(range/2);i++) {
+		if(subset(num,dp,i,n)) {
+			int temp = range - (2*i);
+			mindiff = min(mindiff,temp);
+		}
+	}
+	
+	cout<<mindiff<<endl;
+	
+	return 0;
+}
+
+*/
+
