ab425ea3ac96eb098c1e53c703a83f6d6552b6f2
SandeepMaithani <sandeep1137@gmail.com>
2020-06-11 21:37:32 +0530

    Min no of insertion and deletion to convert A to B

---
+++ Algo/Dynamic_Programming/Min no of insertion and deletion to convert A to B.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Bottom - Up
+
+int main() {
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string a,b;
+	cin>>a>>b;
+	
+	int n = a.length();
+	int m = b.length();
+	
+	vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+	
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=m;j++) {
+			if(a[i-1] == b[j-1]) {
+				dp[i][j] = 1 + dp[i-1][j-1];
+			}
+			else {
+				int first = dp[i][j-1];
+				int second = dp[i-1][j];
+				
+				dp[i][j] = max(first,second);
+			}
+		}
+	}
+	int lcslen = dp[n][m]; 
+	
+	int deletion = n - lcslen;
+	int insertion = m - lcslen;
+	
+	cout<<"Deletion : "<<deletion<<" Insertion : "<<insertion;
+	
+	return 0;
+}
+
+
+//Recursion + Memoization
+
+/*
+
+int LCS(string a,string b,vector<vector<int>>&dp,int n,int m) {
+	
+	if(n == 0 || m == 0) {
+		
+		dp[n][m] = 0;
+		return 0;
+	}
+	
+	if(dp[n][m] != -1) {
+		return dp[n][m];
+	}
+	
+	if(a[n-1] == b[m-1]) {
+		dp[n][m] = 1+LCS(a,b,dp,n-1,m-1);
+		return dp[n][m];
+	}
+	else {
+		
+		int first = LCS(a,b,dp,n,m-1);
+		int second = LCS(a,b,dp,n-1,m);
+		
+		dp[n][m] = max(first,second);
+		
+		return dp[n][m];
+	}
+}
+
+int main() {
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	string a,b;
+	cin>>a>>b;
+	
+	int n = a.length();
+	int m = b.length();
+	
+	vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+	
+	int lcslen = LCS(a,b,dp,n,m); 
+	
+	int deletion = n - lcslen;
+	int insertion = m - lcslen;
+	
+	cout<<"Deletion : "<<deletion<<" Insertion : "<<insertion;
+	
+	return 0;
+}
+
+*/
