b7d1ca9bb840653968981b75298bc42163fbc450
SandeepMaithani <sandeep1137@gmail.com>
2020-06-26 19:29:57 +0530

    Diameter of a binary tree

---
+++ Algo/Dynamic_Programming/Diameter of a binary tree.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+	int data;
+	TreeNode* left;
+	TreeNode* right;
+
+	Node(int x) {
+		data = x;
+		left = right = NULL;
+	}
+};
+
+int diameter(TreeNode* Root, int &answer) {
+
+	if (Root == NULL) {
+		return 0;
+	}
+
+	int ltdia = diameter(Root -> left, answer);
+	int rtdia = diameter(Root -> right, answer);
+
+	int temp = max(ltdia, rtdia) + 1;
+
+	int solution = left + right + 1;
+
+	answer = max(answer, solution);
+
+	return temp;
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	//Root is a pointer of type struct TreeNode which will point to root node of the tree
+
+	int answer = INT_MIN;
+
+	int temp = diameter(Root, &answer);
+
+	cout << answer << endl;
+
+	return 0;
+
+}
