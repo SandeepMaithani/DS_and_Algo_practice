5ef1e16c1f70ee69a96777228ad02aa190d7c5c2
SandeepMaithani <sandeep1137@gmail.com>
2020-07-02 00:43:21 +0530

    Find an Element in a Rotated Sorted Array

---
+++ Algo/Binary_Search/Find an Element in a Rotated Sorted Array.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int bsearch(vector<int>& nums, int target, int start, int end) {
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+        else if (nums[mid] > target) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int getind(vector<int>& nums, int start, int end) {
+    int answer = -1;
+    int n = nums.size();
+    while (start <= end) {
+        int mid = start + (end - start) / 2 ;
+
+        int prev = (mid + n - 1) % n;
+        int next = (mid + 1) % n;
+
+        if (nums[mid] < nums[prev] && nums[mid] < nums[next]) {
+            answer = mid;
+            break;
+        }
+        else if (nums[mid] < nums[end]) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+    }
+    return answer;
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, target;
+    cin >> n >> target;
+
+    vector<int>nums(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    if (nums.size() == 0) {
+        return -1;
+    }
+
+    if (nums.size() == 1) {
+        if (nums[0] == target) {
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    int answer;
+    int minind = getind(nums, 0, nums.size() - 1);
+
+    if (nums[minind] == target) {
+        answer = minind;
+    }
+    else {
+
+        int lt = bsearch(nums, target, 0, minind - 1);
+        int rt = bsearch(nums, target, minind + 1, nums.size() - 1);
+
+        if (lt == -1 && rt == -1) {
+            answer = -1;
+        }
+        else if (lt == -1) {
+            answer = rt;
+        }
+        else {
+            answer = lt;
+        }
+    }
+
+    cout << answer << endl;
+
+    return 0;
+}
