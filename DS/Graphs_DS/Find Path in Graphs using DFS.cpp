1820f205a3cd77a264ed51f19f8c3d002379b169
SandeepMaithani <sandeep1137@gmail.com>
2020-10-12 00:34:33 +0530

    Has Path?

--- DS/Graphs_DS/Find Path in Graphs using DFS.cpp
+++ DS/Graphs_DS/Find Path in Graphs using DFS.cpp
@@ -4,12 +4,35 @@ using namespace std;
 #define endl "\n"
 typedef long long int ll;
 
+bool hasPath(vector<vector<int>>&graph, vector<bool> &visited, int source, int destination) {
+	if (source == destination) {
+		return true;
+	}
+
+	visited[source] = true;
+
+	for (int i = 0; i < graph[source].size(); i++) {
+		int neighbor = graph[source][i];
+
+		if (visited[neighbor] == false) {
+			bool pathPossible = hasPath(graph, visited, neighbor, destination);
+
+			if (pathPossible == true) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
 int main() {
 
-	int vertexCount, edgeCount;
+	int vertexCount, edgeCount, source, destination;
 	cin >> vertexCount >> edgeCount;
 
-	vector<vector<int>>graph(vertexCount, vector<int>);
+	vector<vector<int>>graph(vertexCount);
+	vector<bool>visited(vertexCount, false);
 
 	for (int i = 0; i < edgeCount; i++) {
 		int vertex1, vertex2;
@@ -18,6 +41,8 @@ int main() {
 		graph[vertex1].push_back(vertex2);
 		graph[vertex2].push_back(vertex1);
 	}
+
+	cout << hasPath(graph, visited, source, destination) << endl;
 
 	return 0;
 }
