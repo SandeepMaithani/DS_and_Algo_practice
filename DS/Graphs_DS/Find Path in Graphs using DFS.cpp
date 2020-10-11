a0c52b9b50a8ceaf152ea11c3c2b27ca22374d82
SandeepMaithani <sandeep1137@gmail.com>
2020-10-11 23:52:34 +0530

    Has Path?

--- DS/Graphs_DS/Find Path in Graphs using DFS.cpp
+++ DS/Graphs_DS/Find Path in Graphs using DFS.cpp
@@ -3,3 +3,21 @@ using namespace std;
 #define MOD 1000000007
 #define endl "\n"
 typedef long long int ll;
+
+int main() {
+
+	int vertexCount, edgeCount;
+	cin >> vertexCount >> edgeCount;
+
+	vector<vector<int>>graph(vertexCount, vector<int>);
+
+	for (int i = 0; i < edgeCount; i++) {
+		int vertex1, vertex2;
+		cin >> vertex1 >> vertex2;
+
+		graph[vertex1].push_back(vertex2);
+		graph[vertex2].push_back(vertex1);
+	}
+
+	return 0;
+}
