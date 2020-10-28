ff46c5c1ba3c9752d4230497de11d680251af401
SandeepMaithani <sandeep1137@gmail.com>
2020-10-28 23:51:12 +0530

    Dijkstra Algorithm in Graphs

--- DS/Graphs_DS/Dijkstra Algorithm in Graphs.cpp.cpp
+++ DS/Graphs_DS/Dijkstra Algorithm in Graphs.cpp.cpp
@@ -4,7 +4,38 @@ using namespace std;
 #define endl "\n"
 typedef long long int ll;
 
-void dijkstraAlgo(vector <vector<int>>& graph, int person, int time) {
+struct CompareWeight {
+	bool operator()(pair<int, int> const& p1, pair<int, int> const& p2)
+	{
+		return p1.second > p2.second;
+	}
+};
+
+
+void dijkstraAlgo(vector<vector<pair<int, int>>>& graph, vector<bool>& visited, int source, int &cost) {
+	priority_queue<pair<int, int>, vector<pair<int, int>>, CompareWeight>minHeap;
+	minHeap.push({source, 0});
+
+	while (!minHeap.empty()) {
+		pair<int, int>curVertexInfo = minHeap.top();
+		minHeap.pop();
+
+		int curVertex = curVertexInfo.first;
+		int weight = curVertexInfo.second;
+
+		if (visited[curVertex] == true) {
+			continue;
+		}
+
+		visited[curVertex] = true;
+		cost = weight;
+
+		for (auto neighbour : graph[curVertex]) {
+			if (visited[neighbour.first] == false) {
+				minHeap.push({neighbour.first, weight + neighbour.second});
+			}
+		}
+	}
 }
 
 int main() {
@@ -19,19 +50,25 @@ int main() {
 	freopen("output1.txt", "w", stdout);
 #endif
 
-	int vertexCount, edgeCount, person, time;
+	int vertexCount, edgeCount, source, cost = 0;
 	cin >> vertexCount >> edgeCount;
 
-	vector<vector<int>>graph(vertexCount);
-	vector<int>traversal;
+	vector<vector<pair<int, int>>>graph(vertexCount);
+	vector<bool>visited(vertexCount, false);
 
 	for (int i = 0; i < edgeCount; i++) {
 		int vertex1, vertex2, weight;
 		cin >> vertex1 >> vertex2 >> weight;
 
-		graph[vertex1].push_back(vertex2);
-		graph[vertex2].push_back(vertex1);
+		graph[vertex1].push_back({vertex2, weight});
+		graph[vertex2].push_back({vertex1, weight});
 	}
+
+	cin >> source;
+
+	dijkstraAlgo(graph, visited, source, cost);
+
+	cout << cost << endl;
 
 	return 0;
 }
