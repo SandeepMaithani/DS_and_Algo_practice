363f8a6ff1b97d87fb4ddb0b6dd8a6a425a69864
SandeepMaithani <sandeep1137@gmail.com>
2021-01-22 23:30:48 +0530

    Climbing Stairs with Jumps

---
+++ Algo/Basic_DP_Problem/Climbing Stairs with Jumps.cpp
@@ -0,0 +1,72 @@
+/*
+
+Question Statement :-
+
+In the Climb Stairs with jumps problem:
+
+1. You are given a number N, representing the number of stairs in a staircase.
+2. You are on the 0th step and are required to climb to the top.
+3. You are given N numbers, where ith element's value represents - till how far from the step  you could jump to in a single move.
+4. You are required to print the number of different paths via which you can climb to the top.
+
+*/
+
+
+/*
+
+Complexity Analysis :-
+
+Time Complexity: O(N*N). Here it is possible that one can jump to each subsequent steps so we can say in worst case one can jump to
+all subsequent steps from each step, so time complexity will be O(N*N).
+
+Space Complexity: O(N).To store the values in a DP vector of length N, so N extra space is needed.
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define endl "\n"
+typedef long long int ll;
+
+int countWaysMemo(int )
+
+int countWaysTab(int n, vector<int>&jump, vector<int>&dp) {
+	dp[n] = 1;
+
+	for (int i = n - 1; i >= 0; i--) {
+		for (int j = 1; j <= jump[i] && i + j <= n; j++) {
+			dp[i] += dp[i + j];
+		}
+	}
+
+	return dp[0];
+}
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<int>jump(n);
+
+	for (int i = 0 ; i < n ; i++) {
+		cin >> jump[i];
+	}
+
+	vector<int>dp(n + 1, 0);
+
+	int answer = countWaysTab(n, jump, dp);
+
+	cout << answer << endl;
+
+	return 0;
+}
+
+
+
